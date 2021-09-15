15c7852713cc8a6e11646fc4b578e93d1aa7344f
Víctor Vanaclocha Cebrián <victorvace@gmail.com>
2021-09-15 22:40:15 +0200

    Ejercicio 8 (20 min)

--- src/main/java/es/geekshubs/academy/Milliseconds.java
+++ src/main/java/es/geekshubs/academy/Milliseconds.java
@@ -1,5 +1,7 @@
 package es.geekshubs.academy;
 
+import java.util.concurrent.TimeUnit;
+
 public class Milliseconds {
 
   /**
@@ -10,7 +12,16 @@ public class Milliseconds {
    * @return
    */
   public int apply(int h, int m, int s) {
-    return 0;
+
+    if (h < 0 || h > 23 || m < 0 || m > 59 || s < 0 || s > 59) {
+      return -1;
+    }
+
+    long hours = TimeUnit.HOURS.toMillis(h);
+    long minutes = TimeUnit.MINUTES.toMillis(m);
+    long seconds = TimeUnit.SECONDS.toMillis(s);
+
+    return (int) (hours + minutes + seconds);
   }
 
 
